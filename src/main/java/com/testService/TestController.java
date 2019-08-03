56e313990a1073212a7a422a8efd0ba1c09ff71d
Maiara <maiara@ciclic.com.br>
2019-08-03 14:40:29 -0300

    correcoes do checkstyle

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -4,10 +4,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
 @CrossOrigin
 public class TestController {
 
+
+	/**
+	 *
+	 * @return a
+	 */
 	@GetMapping("/home")
 	public String homeRequest() {
 		return "Hello World!";
